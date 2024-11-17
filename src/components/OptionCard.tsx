273d90dddbc21d4a852dd9360cacea8906e71b84
copws <copcin@yeah.net>
2024-11-17 10:52:41 +0800

    0.1.0

--- src/components/OptionCard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Space } from "antd";
-
-const OptionCard = (props: { children?: React.ReactNode }) => (
-  <Space style={{ marginTop: "5px" }}>{props.children}</Space>
-);
-
-export default OptionCard;
